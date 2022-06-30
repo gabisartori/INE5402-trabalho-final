c158132092d61193f2c107524dc29034977335ff
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-06-30 15:50:37 -0300

    ble

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ from posts.TextPost import *
 def register_user(user_list):
     name = input("Enter your name: ")
     private = input("Is your profile private? (y/n): ").strip()[0].lower() == 'y'
-    user = User(name, private)
+    user = User(name, len(user_list), private)
     user_list.append(user)
     print("User registered successfully!")
 
+# :?:??????
+def set_post_list(user_list):
+    temp = []
+    for user in user_list:
+        for post in user.posts:
+            temp.append(post)
+
+    return temp
 
 def main():
     user_list = []
-    post_list = []
     register_user(user_list)
-
+    post_list = set_post_list(user_list)
     eu = user_list[0]
 
     post = TextPost(eu, "Hello World!", "This is my first post!")
