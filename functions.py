38c3c1b7a1719f5eb8a9b4ea1c251a551e37ba19
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-03 20:21:50 -0300

    organizar código e funções para mostrar posts
    Funções foram movidas para um arquivo próprio, e funções foram definidas para montar a timeline de um usuário, considerando posts feitos por contas privadas

---
+++ functions.py
@@ -0,0 +1,42 @@
+from User import *
+from posts.Post import *
+from posts.TextPost import *
+
+
+def register_user(user_list):
+    name = input("Enter your name: ")
+    private = input("Is your profile private? (y/n): ").strip()[0].lower() == 'y'
+    user = User(name, len(user_list), private)
+    user_list.append(user)
+    print("User registered successfully!")
+
+
+def queryPost(id, post_list):
+    for post in post_list:
+        if post.id == id:
+            return post
+
+# :?:??????
+def set_post_list(user_list):
+    temp = []
+    for user in user_list:
+        for post in user.posts:
+            temp.append(post)
+
+    return temp
+
+def show_post_replies(post):
+    pass
+
+def show_post(post: Post):
+    print('='*30)
+    if post.title:
+        print(post.owner.name + ": " + post.title)
+    print(post.content)
+    print("Likes: " + str(post.likes))
+    show_post_replies(post)
+    
+def show_timeline(user, posts):
+    for post in posts:
+        if not post.private or post.owner == user:
+            show_post(post)
--- main.py
+++ main.py
@@ -1,39 +1,14 @@
 from User import *
 from posts.Post import *
 from posts.TextPost import *
-
-def register_user(user_list):
-    name = input("Enter your name: ")
-    private = input("Is your profile private? (y/n): ").strip()[0].lower() == 'y'
-    user = User(name, len(user_list), private)
-    user_list.append(user)
-    print("User registered successfully!")
-
-
-def queryPost(id, post_list):
-    for post in post_list:
-        if post.id == id:
-            return post
-
-# :?:??????
-def set_post_list(user_list):
-    temp = []
-    for user in user_list:
-        for post in user.posts:
-            temp.append(post)
-
-    return temp
+from functions import *
 
 def main():
     user_list = []
     register_user(user_list)
-    post_list = set_post_list(user_list)
-    eu = user_list[0]
-
-    post = TextPost(eu, "Hello World!", "This is my first post!")
-    eu.add_post(post)
-
-    for post in eu.posts:
-        print(post.private)
+    me = user_list[0]
+    me.add_post(TextPost(me, "Hello World!", "This is my first post!"))
+    me.comment(me.posts[0], "This is a comment!")
+    show_timeline(me, me.posts)
 
 if __name__ == '__main__': main()
