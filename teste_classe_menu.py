18be5a941f549982dd6230b873c68c5d4398c1a6
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-09 22:14:25 -0300

    Finalizando transcrição da classe do menu e comentários
    O que já estava feito do módulo menu foi refatorado pra classe Menu. Menu de login, cadastro prontos, menu de administrador parcialmente pronto e menu de usuário a ser implementado.

--- teste_classe_menu.py
+++ teste_classe_menu.py
@@ -13,6 +13,10 @@ class Menu:
 [4] Logout
     '''
 
+    admin_name = 'admin'
+    admin_password = 'admin'
+    password_hash = sha256(admin_password)
+
     def __init__(self, user_list):
         self.user_list = user_list
     
@@ -23,10 +27,13 @@ class Menu:
             print(self.menu_options)
             option = input('Digite a opção desejada: ')
             if option == '1':
+                # Inicia o menu de login
                 self.login_menu(self.user_list)
             elif option == '2':
+                # Inicia o menu de cadastro
                 self.register_menu(self.user_list)
             elif option == '3':
+                # Encerra o programa
                 print('Programa encerrado.')
                 break
             else:
@@ -39,17 +46,17 @@ class Menu:
             print('Para cadastrar um usuário, digite "2" no menu principal.')
             return
         
-        admin_name = 'admin'
-        admin_password = 'admin'
-        admin_hash = sha256(admin_password)
+        
         name = input('Insira o nome de usuário: ')
         password = input('Insira a senha: ')
         for user in user_list:
-            if name == admin_name and sha256(password) == admin_hash:
+            if name == self.admin_name and sha256(password) == self.password_hash:
+                # Incia o menu de administrador
                 self.admin_menu(user_list)
                 return
             elif user.name == name and user.password == password:
                 print('Login realizado com sucesso!')
+                # Incia o menu do usuário logado
                 self.user_menu(user)
                 return
         
@@ -57,13 +64,59 @@ class Menu:
     
 
     def register_menu(self):
+        # Coleta os dados do usuário
         name = input("Insira seu nome de usuário: ")
         private = input("Essa conta será privada? (s/n): ").strip()[0].lower() == 's'
         password = input("Insira sua senha: ")
         if password == input('Confira sua senha: '):
+            # Cria o usuário e o adiciona na lista de usuários do programa
             user = User(name, len(self.user_list), private, password)
             self.user_list.append(user)
             print("User registered successfully!")
         else:
             print('Senhas não conferem! Tente novamente.')
             register_user(self.user_list)
+    
+    def admin_menu(self):
+        while True:
+            print(self.admin_options)
+            option = input('Digite a opção desejada: ')
+            if option == '1':
+                # Incia o menu de deletar usuário
+                self.delete_user(self.user_list)
+            elif option == '2':
+                # Incia o menu de deletar publicação
+                self.delete_post()
+            elif option == '3':
+                # Incia o menu de listar usuários
+                self.list_users(self.user_list)
+            elif option == '4':
+                # Encerra o menu de administrador
+                break
+            else:
+                print('Opção inválida!')
+    
+    def delete_user(self):
+        # Coleta e confirma o nome do usuário a ser deletado
+        name = input('Insira o nome de usuário que queres deletar: ')
+        if not name == input('Confira o nome de usuário: '):
+            print('Nomes não conferem! Tente novamente.')
+            return        
+        confirm = input('Tem certeza que deseja deletar o usuário? (s/n): ').strip()[0].lower() != 's'
+        if confirm: return
+        # Busca e remove o usuário da lista de usuários do programa
+        for user in self.user_list:
+            if user.name == name:
+                self.user_list.remove(user)
+                print('Usuário deletado com sucesso!')
+                return
+
+    def delete_post(self):
+        pass
+
+    def list_users(self):
+        for user in self.user_list:
+            print(user.name)
+
+    def user_menu(self):
+        pass
